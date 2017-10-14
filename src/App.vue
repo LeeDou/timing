e168937236dbeb756d5128591c1818794cc6b42e
leedou <1040241228@qq.com>
2017-10-14 12:03:00 +0800

    [+]提交

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <count-down :start='start' @countDown ='start=false' @click.native='sendCode'></count-down>
+    </div>
+</template>
+<script>
+import CountDown from './components/CountDown.vue'
+    export default {
+        components: {
+          CountDown 
+        },
+        data () {
+            return {
+                start : false
+            }
+        },
+        methods : {
+            sendCode (value) {
+                //前面发送ajax请求成功之后调用this.start = true开始短信倒计时
+                this.start = true
+            }
+        }
+    }
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/CountDown.vue
@@ -0,0 +1,77 @@
+<template>
+     <div>
+        <button>{{time | change}}</button>
+     </div>
+</template>
+
+<script>
+let flag = false
+    export default {
+        data () {
+            return {
+                time : '获取验证码',
+            }
+        },
+        props : {
+            start : {
+                type : Boolean
+            }
+        },
+        watch : {
+            start (value,oldvalue) {
+                if(value == true){
+                    this.countDown()
+                }
+            }
+        },
+        methods: {
+            countDown () {
+                this.time = 60;
+                let time = setInterval(()=>{
+                    this.time --
+                    if(this.time == 0){
+                        this.$emit('countDown')
+                        this.time = '获取验证码'
+                        flag = true
+                        clearInterval(time)
+                    }
+                },100)
+            }
+        },
+        filters : {
+            change (value) {
+               if(!value) return ""
+               if(!isNaN(value)){
+                   if(flag == true){
+                       return `重新发送${value}S`
+                   }
+                   return value+'S后可重新发送验证码'
+               }else{
+                   return value
+               }
+            }
+        }
+
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
